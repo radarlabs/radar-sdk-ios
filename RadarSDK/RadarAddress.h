349ed1dcd898fb45687ec6629fff3077d56436b5
Nick Patrick <nicholasjohnpatrick@gmail.com>
2020-01-08 12:44:56 -0500

    bump version and update docs

--- RadarSDK/RadarAddress.h
+++ RadarSDK/RadarAddress.h
@@ -29,62 +29,62 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  The latitude and longitude of the address.
  */
-@property(nonnull, copy, nonatomic, readonly) RadarCoordinate *coordinate;
+@property (nonnull, copy, nonatomic, readonly) RadarCoordinate *coordinate;
 
 /**
  The formatted string representation of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *formattedAddress;
+@property (nullable, copy, nonatomic, readonly) NSString *formattedAddress;
 
 /**
  The name of the country of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *country;
+@property (nullable, copy, nonatomic, readonly) NSString *country;
 
 /**
  The unique code of the country of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *countryCode;
+@property (nullable, copy, nonatomic, readonly) NSString *countryCode;
 
 /**
  The flag of the country of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *countryFlag;
+@property (nullable, copy, nonatomic, readonly) NSString *countryFlag;
 
 /**
  The name of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *state;
+@property (nullable, copy, nonatomic, readonly) NSString *state;
 
 /**
  The unique code of the state of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *stateCode;
+@property (nullable, copy, nonatomic, readonly) NSString *stateCode;
 
 /**
  The postal code of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *postalCode;
+@property (nullable, copy, nonatomic, readonly) NSString *postalCode;
 
 /**
  The city of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *city;
+@property (nullable, copy, nonatomic, readonly) NSString *city;
 
 /**
  The borough of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *borough;
+@property (nullable, copy, nonatomic, readonly) NSString *borough;
 
 /**
  The county of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *county;
+@property (nullable, copy, nonatomic, readonly) NSString *county;
 
 /**
  The neighborhood of the address.
  */
-@property(nullable, copy, nonatomic, readonly) NSString *neighborhood;
+@property (nullable, copy, nonatomic, readonly) NSString *neighborhood;
 
 /**
  The number of the address.
