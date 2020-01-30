750ae837ffbee192cd4e8ce86bf1efcfb2582677
cory pisano <cory.pisano@gmail.com>
2020-01-30 17:46:39 -0500

    add header for RadarContext

---
+++ RadarSDK/RadarContext.h
@@ -0,0 +1,78 @@
+//
+//  RadarContext.h
+//  RadarSDK
+//
+//  Copyright © 2020 Radar Labs, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "RadarGeofence.h"
+#import "RadarPlace.h"
+#import "RadarRegion.h"
+
+/**
+ TODO(coryp): RadarContext description and documentation reference
+
+ @see https://radar.io/documentation
+ */
+@interface RadarContext : NSObject
+
+/**
+ The datetime when the location update occurred.
+ */
+@property (nonnull, copy, nonatomic, readonly) NSDate *updatedAt;
+
+/**
+ A boolean indicating whether the location context was generated with your live API key.
+ */
+@property (assign, nonatomic, readonly) BOOL live;
+
+/**
+ The location of the update. Includes coordinates and accuracy
+ */
+@property (nonnull, strong, nonatomic, readonly) CLLocation *location;
+
+/**
+ An array of the geofences the location is in. May be `nil` or empty if the location is not in any geofences.
+ 
+ @see https://radar.io/documentation/geofences
+ */
+@property (nullable, copy, nonatomic, readonly) NSArray<RadarGeofence *> *geofences;
+
+/**
+ The place a location is at. May be `nil` if the location is not at a place, or if Places is not enabled.
+ 
+ @see https://radar.io/documentation/places
+ */
+@property (nullable, copy, nonatomic, readonly) RadarPlace *place;
+
+/**
+ The location's country. May be `nil` if country is not available or if Regions is not enabled.
+ 
+ @see https://radar.io/documentation/regions
+ */
+@property (nullable, strong, nonatomic, readonly) RadarRegion *country;
+
+/**
+ The location's state. May be `nil` if state is not available or if Regions is not enabled.
+ 
+ @see https://radar.io/documentation/regions
+ */
+@property (nullable, strong, nonatomic, readonly) RadarRegion *state;
+
+/**
+ The location's last known designated market area (DMA). May be `nil` if DMA is not available or if Regions is not enabled.
+ 
+ @see https://radar.io/documentation/regions
+ */
+@property (nullable, strong, nonatomic, readonly) RadarRegion *dma;
+
+/**
+ The location's last known postal code. May be `nil` if postal code is not available or if Regions is not enabled.
+ 
+ @see https://radar.io/documentation/regions
+ */
+@property (nullable, strong, nonatomic, readonly) RadarRegion *postalCode;
+
+@end
