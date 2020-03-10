f52de66da6ce27ab6a4e3d8723958ba62c96ba04
Craig Kochis <cjkochis@gmail.com>
2020-03-10 15:15:02 -0400

    support splitting "name" into "addressLabel" and "placeLabel"

--- dist/RadarSDK.framework/Versions/A/Headers/RadarAddress.h
+++ dist/RadarSDK.framework/Versions/A/Headers/RadarAddress.h
@@ -57,7 +57,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nullable, copy, nonatomic, readonly) NSString *countryFlag;
 
 /**
- The name of the address.
+ The name of the state.
  */
 @property (nullable, copy, nonatomic, readonly) NSString *state;
 
@@ -97,9 +97,14 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nullable, copy, nonatomic, readonly) NSString *number;
 
 /**
- The name of the address.
+ The label of the address.
  */
-@property (nullable, copy, nonatomic, readonly) NSString *name;
+@property (nullable, copy, nonatomic, readonly) NSString *addressLabel;
+
+/**
+ The label of the place.
+ */
+@property (nullable, copy, nonatomic, readonly) NSString *placeLabel;
 
 /**
   The confidence level of the geocoding result.
