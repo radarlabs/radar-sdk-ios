abe24ef6e1f42301bda880b38107642f34e5fa54
Nick Patrick <nicholasjohnpatrick@gmail.com>
2020-07-21 18:23:22 -0400

    update sample code

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -78,8 +78,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             print("Reverse geocode: status = \(Radar.stringForStatus(status)); formattedAddress = \(String(describing: addresses?.first?.formattedAddress))")
         }
 
-        Radar.ipGeocode { (status, address) in
-            print("IP geocode: status = \(Radar.stringForStatus(status)); country = \(String(describing: address?.countryCode)); city = \(String(describing: address?.city))")
+        Radar.ipGeocode { (status, address, proxy) in
+            print("IP geocode: status = \(Radar.stringForStatus(status)); country = \(String(describing: address?.countryCode)); city = \(String(describing: address?.city)); proxy = \(proxy)")
         }
         
         let origin = CLLocation(latitude: 40.78382, longitude: -73.97536)
