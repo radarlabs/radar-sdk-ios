1569582a1ed6d075ee5a5c69be4470839fee064a
Nick Patrick <nicholasjohnpatrick@gmail.com>
2020-12-29 15:28:31 -0500

    & not ?

--- RadarSDK/RadarAPIClient.m
+++ RadarSDK/RadarAPIClient.m
@@ -63,7 +63,7 @@
 
     NSMutableString *queryString = [NSMutableString new];
     [queryString appendFormat:@"installId=%@", [RadarSettings installId]];
-    [queryString appendFormat:@"?sessionId=%@", [RadarSettings sessionId]];
+    [queryString appendFormat:@"&sessionId=%@", [RadarSettings sessionId]];
     NSString *locationAuthorization = [RadarUtils locationAuthorization];
     if (locationAuthorization) {
         [queryString appendFormat:@"&locationAuthorization=%@", locationAuthorization];
