78fc9d18cd2f511fffadd7aaa9608061ee590768
Nick Patrick <nicholasjohnpatrick@gmail.com>
2021-01-27 10:55:30 -0500

    add NOT_DETERMINED value for locationAuthorization

--- RadarSDK/RadarUtils.m
+++ RadarSDK/RadarUtils.m
@@ -76,8 +76,10 @@
         return @"GRANTED_FOREGROUND";
     case kCLAuthorizationStatusAuthorizedAlways:
         return @"GRANTED_BACKGROUND";
-    default:
+    case kCLAuthorizationStatusDenied:
         return @"DENIED";
+    default:
+        return @"NOT_DETERMINED";
     }
 }
 
