f79801afb55e4d7f12591b177c818bd2062c8a2b
Nick Patrick <nicholasjohnpatrick@gmail.com>
2021-04-10 13:04:06 -0400

    zip xcframework

--- dist/RadarSDK.xcframework/ios-arm64_i386_x86_64-simulator/RadarSDK.framework/Headers/RadarSDK.h
+++ dist/RadarSDK.xcframework/ios-arm64_i386_x86_64-simulator/RadarSDK.framework/Headers/RadarSDK.h
@@ -13,8 +13,6 @@ FOUNDATION_EXPORT double RadarSDKVersionNumber;
 //! Project version string for RadarSDK.
 FOUNDATION_EXPORT const unsigned char RadarSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <RadarSDK/PublicHeader.h>
-
 #import "Radar.h"
 #import "RadarAddress.h"
 #import "RadarChain.h"
