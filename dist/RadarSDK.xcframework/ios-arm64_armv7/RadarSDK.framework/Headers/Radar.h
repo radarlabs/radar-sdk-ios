231e175f4dd1b5b23b72190664a4c841eb986eca
Nick Patrick <nicholasjohnpatrick@gmail.com>
2021-09-05 12:34:27 -0400

    revert dist/

--- dist/RadarSDK.xcframework/ios-arm64_armv7/RadarSDK.framework/Headers/Radar.h
+++ dist/RadarSDK.xcframework/ios-arm64_armv7/RadarSDK.framework/Headers/Radar.h
@@ -145,14 +145,11 @@ typedef void (^_Nullable RadarLocationCompletionHandler)(RadarStatus status, CLL
 /**
  Called when a beacon ranging request succeeds, fails, or times out.
 
- Receives the request status and, if successful, the nearby beacon identifiers, RSSI, and proximity.
+ Receives the request status and, if successful, the nearby beacon identifiers.
 
  @see https://radar.io/documentation/beacons
  */
-typedef void (^_Nullable RadarBeaconCompletionHandler)(RadarStatus status,
-                                                       NSArray<NSString *> *_Nullable nearbyBeaconIdentifiers,
-                                                       NSDictionary *_Nullable nearbyBeaconRSSI,
-                                                       NSDictionary *_Nullable nearbyBeaconProximity);
+typedef void (^_Nullable RadarBeaconCompletionHandler)(RadarStatus status, NSArray<NSString *> *_Nullable nearbyBeacons);
 
 /**
  Called when a track request succeeds, fails, or times out.
