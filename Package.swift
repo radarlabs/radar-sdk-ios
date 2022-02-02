f7436db2e7b7bca30594c0a951844814f24b1acc
Jason R Tibbetts <jason@radar.com>
2022-02-02 12:24:57 -0500

    8281: [project] Restored the Package.swift file. Moved the model and internal-only .m files into the Internal folder.

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "RadarSDK",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "RadarSDK",
+            targets: ["RadarSDK"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "RadarSDK",
+            path: "RadarSDK",
+            exclude: ["Info.plist", "RadarSDK.h"],
+            publicHeadersPath: "Include",
+            cSettings: [
+                .headerSearchPath("Internal")
+            ]
+        )
+    ]
+)
