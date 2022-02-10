4ae909e2ce0f8bd2de1cba3b8d68ffd3c7a8d0b8
Jason R Tibbetts <jason@radar.com>
2022-02-10 14:25:49 -0500

    [sc-8281] [add] Added a basic UI that displays the SDK's version number.

--- DistributionExamples/SwiftAppWithCocoaPods/SwiftAppWithCocoaPods/ViewController.swift
+++ DistributionExamples/SwiftAppWithCocoaPods/SwiftAppWithCocoaPods/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var versionNumberLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        let radarBundle = Bundle(for: Radar.self)
+        let versionString = radarBundle.infoDictionary!["CFBundleShortVersionString"] as? String
+        versionNumberLabel.text = versionString
     }
 
 }
