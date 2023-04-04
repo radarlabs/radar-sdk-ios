e2c993be7a392bce167e5963b88f965d2851f27a
Nick Patrick <nicholasjohnpatrick@gmail.com>
2023-04-04 17:34:01 -0400

    clean up some comments

--- RadarSDK/CLLocation+Radar.h
+++ RadarSDK/CLLocation+Radar.h
@@ -12,11 +12,6 @@
 
 @interface CLLocation (Radar)
 
-/**
- YES if the location's latitude is between -180.0 and 180.0 (except 0.0)),
- longitude is between -90.0 and 90.0 (except 0.0), and horizontalAccuracy is
- greater than 0.
- */
 @property (nonatomic, readonly) BOOL isValid;
 
 @end
