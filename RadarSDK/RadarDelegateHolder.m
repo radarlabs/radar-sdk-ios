e18b672ee05021af7a8adde4c27335aea02801b7
Nick Patrick <nicholasjohnpatrick@gmail.com>
2023-04-28 12:46:35 -0400

    support event notifications

--- RadarSDK/RadarDelegateHolder.m
+++ RadarSDK/RadarDelegateHolder.m
@@ -28,6 +28,8 @@
     if (self.delegate) {
         [self.delegate didReceiveEvents:events user:user];
     }
+    
+    [self showNotificationsForEvents:events];
 
     for (RadarEvent *event in events) {
         [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo
@@ -72,6 +74,59 @@
 - (void)didLogMessage:(NSString *)message {
     if (self.delegate) {
         [self.delegate didLogMessage:message];
+    }
+}
+
+- (void)showNotificationsForEvents:(NSArray<RadarEvent *> *)events {
+    if (!events || !events.count) {
+        return;
+    }
+    
+    for (RadarEvent *event in events) {
+        NSString *notificationText;
+        NSDictionary *metadata;
+        
+        if (event.type == RadarEventTypeUserEnteredGeofence && event.geofence && event.geofence.metadata) {
+            metadata = event.geofence.metadata;
+            notificationText = [metadata objectForKey:@"radar:entryNotificationText"];
+        } else if (event.type == RadarEventTypeUserExitedGeofence && event.geofence && event.geofence.metadata) {
+            metadata = event.geofence.metadata;
+            notificationText = [metadata objectForKey:@"radar:exitNotificationText"];
+        } else if (event.type == RadarEventTypeUserEnteredBeacon && event.beacon && event.beacon.metadata) {
+            metadata = event.beacon.metadata;
+            notificationText = [metadata objectForKey:@"radar:entryNotificationText"];
+        } else if (event.type == RadarEventTypeUserExitedBeacon && event.beacon && event.geofence.metadata) {
+            metadata = event.beacon.metadata;
+            notificationText = [metadata objectForKey:@"radar:exitNotificationText"];
+        } else if (event.type == RadarEventTypeUserApproachingTripDestination && event.trip && event.trip.metadata) {
+            metadata = event.trip.metadata;
+            notificationText = [event.trip.metadata objectForKey:@"radar:approachingNotificationText"];
+        } else if (event.type == RadarEventTypeUserArrivedAtTripDestination && event.trip && event.trip.metadata) {
+            metadata = event.trip.metadata;
+            notificationText = [event.trip.metadata objectForKey:@"radar:arrivalNotificationText"];
+        }
+        
+        if (notificationText) {
+            NSString *identifier = event._id;
+            NSString *categoryIdentifier = [RadarEvent stringForType:event.type];
+            
+            UNMutableNotificationContent *content = [UNMutableNotificationContent new];
+            content.body = [NSString localizedUserNotificationStringForKey:notificationText arguments:nil];
+            content.userInfo = metadata;
+            content.categoryIdentifier = categoryIdentifier;
+
+            UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:nil];
+            [UNUserNotificationCenter.currentNotificationCenter addNotificationRequest:request withCompletionHandler:^(NSError *_Nullable error) {
+                if (error) {
+                    [[RadarLogger sharedInstance]
+                     logWithLevel:RadarLogLevelDebug
+                     message:[NSString stringWithFormat:@"Error adding local notification | identifier = %@; error = %@", request.identifier, error]];
+                } else {
+                    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelDebug
+                                                       message:[NSString stringWithFormat:@"Added local notification | identifier = %@", request.identifier]];
+                }
+            }];
+        }
     }
 }
 
