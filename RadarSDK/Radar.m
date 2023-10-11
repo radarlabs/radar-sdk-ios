ba976647cab4626389423ff92dda56180f26160a
KennyHuRadar <139801512+KennyHuRadar@users.noreply.github.com>
2023-10-11 12:59:16 -0400

    Add rto tracking options update to trackOnce and trackVerified calls (#277)
    * update rto with trackonce
    
    * added rto tracking option update to trackverified
    
    * add check for null
    
    * bump version
    
    * fix formatting;

--- RadarSDK/Radar.m
+++ RadarSDK/Radar.m
@@ -161,6 +161,10 @@
                                                      NSArray<RadarGeofence *> *_Nullable nearbyGeofences, RadarConfig *_Nullable config, NSString *_Nullable token) {
                                      if (status == RadarStatusSuccess) {
                                          [[RadarLocationManager sharedInstance] replaceSyncedGeofences:nearbyGeofences];
+                                         if(config != nil){
+                                             [[RadarLocationManager sharedInstance] updateTrackingFromMeta:config.meta];
+                                         }
+                                         
                                      }
 
                                      if (completionHandler) {
@@ -228,7 +232,9 @@
                                                beacons:nil
                                      completionHandler:^(RadarStatus status, NSDictionary *_Nullable res, NSArray<RadarEvent *> *_Nullable events, RadarUser *_Nullable user,
                                                          NSArray<RadarGeofence *> *_Nullable nearbyGeofences, RadarConfig *_Nullable config, NSString *_Nullable token) {
-
+                                        if (status == RadarStatusSuccess && config != nil) {                                    
+                                            [[RadarLocationManager sharedInstance] updateTrackingFromMeta:config.meta];                                            
+                                        }
                                          if (completionHandler) {
                                              [RadarUtils runOnMainThread:^{
                                                  completionHandler(status, location, events, user);
@@ -272,6 +278,9 @@
                                                        completionHandler:^(RadarStatus status, NSDictionary *_Nullable res, NSArray<RadarEvent *> *_Nullable events,
                                                                            RadarUser *_Nullable user, NSArray<RadarGeofence *> *_Nullable nearbyGeofences,
                                                                            RadarConfig *_Nullable config, NSString *_Nullable token) {
+                                                            if (status == RadarStatusSuccess && config != nil) { 
+                                                                [[RadarLocationManager sharedInstance] updateTrackingFromMeta:config.meta];
+                                                            }
                                                            if (completionHandler) {
                                                                [RadarUtils runOnMainThread:^{
                                                                    completionHandler(status, location, events, user);
@@ -318,6 +327,9 @@
                                                        completionHandler:^(RadarStatus status, NSDictionary *_Nullable res, NSArray<RadarEvent *> *_Nullable events,
                                                                            RadarUser *_Nullable user, NSArray<RadarGeofence *> *_Nullable nearbyGeofences,
                                                                            RadarConfig *_Nullable config, NSString *_Nullable token) {
+                                                            if (status == RadarStatusSuccess && config != nil) { 
+                                                                [[RadarLocationManager sharedInstance] updateTrackingFromMeta:config.meta];
+                                                            }
                                                            if (completionHandler) {
                                                                [RadarUtils runOnMainThread:^{
                                                                    completionHandler(status, token);
