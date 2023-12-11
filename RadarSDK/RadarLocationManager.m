f42edf99d7e3ff5f56bd722afc07b6d292d6c959
Liam Meier <liam.meier@radar.com>
2023-12-11 12:04:41 -0500

    Add buffer to first term, not second

--- RadarSDK/RadarLocationManager.m
+++ RadarSDK/RadarLocationManager.m
@@ -779,7 +779,7 @@ static NSString *const kSyncBeaconUUIDIdentifierPrefix = @"radar_uuid_";
         }
 
         // We add the 0.1 second buffer to account for the fact that the timer may fire slightly before the desired interval
-        if (lastSyncInterval < options.desiredSyncInterval + 0.1) {
+        if (lastSyncInterval + 0.1 < options.desiredSyncInterval) {
             [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelDebug
                                                message:[NSString stringWithFormat:@"Skipping sync: desired sync interval | desiredSyncInterval = %d; lastSyncInterval = %f",
                                                                                   options.desiredSyncInterval, lastSyncInterval]];
