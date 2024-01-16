8992ef6cfc24f0e461115aed9ed1e55030465dbf
York Wei <york.wei@icloud.com>
2024-01-16 13:48:32 -0500

    limit precision when comparing coordinates

--- RadarSDK/CLLocation+Radar.m
+++ RadarSDK/CLLocation+Radar.m
@@ -14,11 +14,15 @@
     CLLocationDegrees lat = self.coordinate.latitude;
     CLLocationDegrees lon = self.coordinate.longitude;
 
-    BOOL latitudeValid = lat != 0.0 && lat > -90.0 && lat < 90.0;
-    BOOL longitudeValid = lon != 0.0 && lon > -180.0 && lon < 180;
+    BOOL latitudeValid = ![self isDouble:lat equalTo:0.0] && lat > -90.0 && lat < 90.0;
+    BOOL longitudeValid = ![self isDouble:lon equalTo:0.0] && lon > -180.0 && lon < 180;
     BOOL horizontalAccuracyValid = self.horizontalAccuracy > 0;
 
     return latitudeValid && longitudeValid && horizontalAccuracyValid;
+}
+
+- (BOOL)isDouble:(double)firstValue equalTo:(double)secondValue {
+    return fabs(firstValue - secondValue) < FLT_EPSILON;
 }
 
 @end
