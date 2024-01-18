3211df73c50bddb780283c97b6847d9143304ab7
York Wei <49313072+york-wei@users.noreply.github.com>
2024-01-18 16:53:57 -0500

    Ignore 0 values of latitude and longitude on client (#297)
    * limit precision when comparing coordinates
    
    * Update CLLocation+Radar.m
    
    * update precision to 8 decimal places
    
    * revert version for testing
    
    * Revert "revert version for testing"
    
    This reverts commit 48bbd47ceeae51d03f3b221f68758f54d831f493.

--- RadarSDK/CLLocation+Radar.m
+++ RadarSDK/CLLocation+Radar.m
@@ -8,17 +8,23 @@
 @import Foundation;
 #import "CLLocation+Radar.h"
 
+const double DEGREE_EPSILON = 0.00000001;
+
 @implementation CLLocation (Radar)
 
 - (BOOL)isValid {
     CLLocationDegrees lat = self.coordinate.latitude;
     CLLocationDegrees lon = self.coordinate.longitude;
 
-    BOOL latitudeValid = lat != 0.0 && lat > -90.0 && lat < 90.0;
-    BOOL longitudeValid = lon != 0.0 && lon > -180.0 && lon < 180;
+    BOOL latitudeValid = ![self isDouble:lat withinDegreeEpsilonTo:0.0] && lat > -90.0 && lat < 90.0;
+    BOOL longitudeValid = ![self isDouble:lon withinDegreeEpsilonTo:0.0] && lon > -180.0 && lon < 180;
     BOOL horizontalAccuracyValid = self.horizontalAccuracy > 0;
 
     return latitudeValid && longitudeValid && horizontalAccuracyValid;
+}
+
+- (BOOL)isDouble:(double)firstValue withinDegreeEpsilonTo:(double)secondValue {
+    return fabs(firstValue - secondValue) < DEGREE_EPSILON;
 }
 
 @end
