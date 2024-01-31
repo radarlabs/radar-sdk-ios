a259dc3cb7b7e21d10eea184fab9b734e606bc98
York Wei <york.wei@icloud.com>
2024-01-31 10:59:14 -0500

    Update RadarLocationManager.m

--- RadarSDK/RadarLocationManager.m
+++ RadarSDK/RadarLocationManager.m
@@ -905,7 +905,7 @@ static NSString *const kSyncBeaconUUIDIdentifierPrefix = @"radar_uuid_";
             }
         }];
     } else {
-        callTrackAPI(nil);
+        callTrackAPI(beacons);
     }
 }
 
