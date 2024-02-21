fcdbd1b83de18ff35133e900ff28a87e620e7b84
Liam Meier <liam.meier@radar.com>
2024-02-21 12:58:28 -0500

    Make responsive mode changes

--- RadarSDK/RadarLocationManager.m
+++ RadarSDK/RadarLocationManager.m
@@ -109,8 +109,8 @@ static NSString *const kSyncBeaconUUIDIdentifierPrefix = @"radar_uuid_";
         _locationManager.allowsBackgroundLocationUpdates = [RadarUtils locationBackgroundMode] && [CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedAlways;
 
         _lowPowerLocationManager = [CLLocationManager new];
-        _lowPowerLocationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers;
-        _lowPowerLocationManager.distanceFilter = 3000;
+        _lowPowerLocationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+        _lowPowerLocationManager.distanceFilter = kCLDistanceFilterNone;
         _lowPowerLocationManager.allowsBackgroundLocationUpdates = [RadarUtils locationBackgroundMode];
 
         _permissionsHelper = [RadarPermissionsHelper new];
