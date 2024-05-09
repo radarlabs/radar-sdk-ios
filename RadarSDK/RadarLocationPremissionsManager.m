cff9319190f63043e696af8049b648cc29fa1155
Kenny Hu <kenny.hu@radar.com>
2024-05-09 12:41:08 -0400

    reduce time to 1 sec

--- RadarSDK/RadarLocationPremissionsManager.m
+++ RadarSDK/RadarLocationPremissionsManager.m
@@ -85,7 +85,7 @@
         [self updateStatus:status];
 
         // Start a timer
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             if (danglingBackgroundPermissionsRequest) {
                 NSLog(@"pop-up did not show");
                 RadarLocationPermissionsStatus *status = [[RadarLocationPermissionsStatus alloc] initWithStatus:self.locationManager.authorizationStatus
