80d1630abe4fab24cea6435dbd11fff12e2b81a3
Nick Patrick <nicholasjohnpatrick@gmail.com>
2024-05-31 11:12:44 -0400

    flush logs

--- RadarSDK/RadarVerificationManager.m
+++ RadarSDK/RadarVerificationManager.m
@@ -14,6 +14,7 @@
 
 #import "RadarVerificationManager.h"
 
+#import "Radar+Internal.h"
 #import "RadarAPIClient.h"
 #import "RadarBeaconManager.h"
 #import "RadarLocationManager.h"
@@ -244,6 +245,8 @@
     
     if (lastTokenElapsed < self.lastToken.expiresIn) {
         [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelDebug message:[NSString stringWithFormat:@"Last token valid | lastToken.expiresIn = %f; lastTokenElapsed = %f", self.lastToken.expiresIn, lastTokenElapsed]];
+        
+        [Radar flushLogs];
         
         return completionHandler(RadarStatusSuccess, self.lastToken);
     }
