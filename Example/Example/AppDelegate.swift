8a9f162f0979f0e7e32237eaae170ad85a3cba68
ShiCheng Lu <shicheng.lu@radar.com>
2024-06-28 10:51:06 -0400

    clean up example

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -26,11 +26,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         Radar.stopTracking()
         
         // Replace with a valid test publishable key
-        Radar.initialize(publishableKey: "prj_test_pk_d9020be31f7cd2357d59a728008f469b1395ee4e")
+        Radar.initialize(publishableKey: "prj_test_pk_0000000000000000000000000000000000000000")
         Radar.setDelegate(self)
         Radar.setVerifiedDelegate(self)
-        
-        return true
 
         if UIApplication.shared.applicationState != .background {
             Radar.getLocation { (status, location, stopped) in
