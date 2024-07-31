21521d13685881547110a90f50834bb54f3d8cce
Kenny Hu <kenny.hu@radar.com>
2024-07-31 15:35:01 -0400

    change header search path

--- Package.swift
+++ Package.swift
@@ -1,46 +1,4 @@
 // swift-tools-version:5.3
-// import PackageDescription
-
-// let package = Package(
-//     name: "RadarSDK",
-//     platforms: [
-//         .iOS(.v10)
-//     ],
-//     products: [
-//         .library(
-//             name: "RadarSDK",
-//             targets: ["RadarSDK"]
-//         ),
-//         .library(
-//             name: "RadarSDKMotion",
-//             targets: ["RadarSDK", "RadarSDKMotion"]
-//         )
-//     ],
-//     dependencies: [],
-//     targets: [
-//         .target(
-//             name: "RadarSDK",
-//             path: "RadarSDK",
-//             exclude: ["Info.plist", "Motion"],
-//             resources: [.process("PrivacyInfo.xcprivacy")],
-//             publicHeadersPath: "Include",
-//             cSettings: [
-//                 .headerSearchPath("."),
-//             ]
-//         ),
-//         .target(
-//             name: "RadarSDKMotion",
-//             dependencies: ["RadarSDK"],
-//             path: "RadarSDK/Motion",
-//             exclude: ["Info.plist"],
-//             publicHeadersPath: "Include",
-//             cSettings: [
-//                 .headerSearchPath("."),
-//             ]
-//         )
-//     ]
-// )
-
 import PackageDescription
 
 let package = Package(
@@ -53,19 +11,62 @@ let package = Package(
             name: "RadarSDK",
             targets: ["RadarSDK"]
         ),
-        
+        .library(
+            name: "RadarSDKMotion",
+            targets: ["RadarSDK", "RadarSDKMotion"]
+        )
     ],
     dependencies: [],
     targets: [
         .target(
             name: "RadarSDK",
             path: "RadarSDK",
-            exclude: ["Info.plist"],
+            exclude: ["Info.plist", "Motion"],
             resources: [.process("PrivacyInfo.xcprivacy")],
             publicHeadersPath: "Include",
             cSettings: [
                 .headerSearchPath("."),
             ]
+        ),
+        .target(
+            name: "RadarSDKMotion",
+            dependencies: ["RadarSDK"],
+            path: "RadarSDK/Motion",
+            exclude: ["Info.plist"],
+            publicHeadersPath: "Include",
+            cSettings: [
+                .headerSearchPath("../Include"),  // Make sure it can find the headers in RadarSDK's Include directory
+                .headerSearchPath(".")  // Include the current directory for RadarSDKMotion headers
+            ]
         )
     ]
 )
+
+// import PackageDescription
+
+// let package = Package(
+//     name: "RadarSDK",
+//     platforms: [
+//         .iOS(.v10)
+//     ],
+//     products: [
+//         .library(
+//             name: "RadarSDK",
+//             targets: ["RadarSDK"]
+//         ),
+        
+//     ],
+//     dependencies: [],
+//     targets: [
+//         .target(
+//             name: "RadarSDK",
+//             path: "RadarSDK",
+//             exclude: ["Info.plist"],
+//             resources: [.process("PrivacyInfo.xcprivacy")],
+//             publicHeadersPath: "Include",
+//             cSettings: [
+//                 .headerSearchPath("."),
+//             ]
+//         )
+//     ]
+// )
