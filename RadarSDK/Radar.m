947b7902ec58bb60dcb8675eb887110db4c1e3d7
KennyHuRadar <139801512+KennyHuRadar@users.noreply.github.com>
2024-09-13 14:03:34 -0400

    change log level reference to that stored in sdkconfiguration (#390)
    * change log level referecne to that stored in sdkconfiguration
    
    * Update RadarSDK/Radar.m
    
    Co-authored-by: Liam Meier <liam.meier@radar.com>
    
    ---------
    
    Co-authored-by: Liam Meier <liam.meier@radar.com>

--- RadarSDK/Radar.m
+++ RadarSDK/Radar.m
@@ -1295,9 +1295,9 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-+ (BOOL)isTestKey {
++ (BOOL)canFlushLogs {
     NSString *publishableKey = [RadarSettings publishableKey];
-    if ([publishableKey hasPrefix:@"prj_test_pk"] || [publishableKey hasPrefix:@"org_test_pk"] || [RadarSettings userDebug]) {
+    if ([publishableKey hasPrefix:@"prj_test_pk"] || [publishableKey hasPrefix:@"org_test_pk"] || [RadarSettings userDebug] || ([RadarSettings sdkConfiguration].logLevel && [RadarSettings sdkConfiguration].logLevel != RadarLogLevelNone)) {
         return YES;
     }
     return NO;
@@ -1308,7 +1308,7 @@
 }
 
 + (void)flushLogs {
-    if (![self isTestKey]) {
+    if (![self canFlushLogs]) {
         return;
     }
 
