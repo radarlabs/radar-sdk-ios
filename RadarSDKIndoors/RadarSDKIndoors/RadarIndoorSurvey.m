5dc2de0a7f0e8d2b11a964e35df0e24290fcce10
gvivan <gvivan6@gmail.com>
2025-06-29 15:48:00 -0400

    refactor RadarIndoorSurvey to use NSLog for logging and integrate RadarSDK for location services

--- RadarSDKIndoors/RadarSDKIndoors/RadarIndoorSurvey.m
+++ RadarSDKIndoors/RadarSDKIndoors/RadarIndoorSurvey.m
@@ -1,8 +1,7 @@
 #import "RadarIndoorSurvey.h"
 #import "NSData+GZIP.h"
-#import "RadarUtils.h"
-#import "RadarLogger.h"
-#import "RadarLocationManager.h"
+@import RadarSDK;
+#import <sys/utsname.h>
 
 @implementation RadarIndoorSurvey
 
@@ -36,11 +35,11 @@
 
 
 - (void)start:(NSString *)placeLabel forLength:(int)surveyLengthSeconds withKnownLocation:(CLLocation *)knownLocation isWhereAmIScan:(BOOL)isWhereAmIScan withCompletionHandler:(RadarIndoorsSurveyCompletionHandler)completionHandler {
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"RadarIndoorSurvey start called with placeLabel: %@, surveyLengthSeconds: %d, isWhereAmIScan: %d", placeLabel, surveyLengthSeconds, isWhereAmIScan]];
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"self.isScanning: %d", self.isScanning]];
+    NSLog(@"RadarIndoorSurvey start called with placeLabel: %@, surveyLengthSeconds: %d, isWhereAmIScan: %d", placeLabel, surveyLengthSeconds, isWhereAmIScan);
+    NSLog(@"self.isScanning: %d", self.isScanning);
 
     if(self.isScanning) {
-        [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelError message:@"Error: start called while already scanning"];
+        NSLog(@"Error: start called while already scanning");
 
         // call callback, pass bad data
         completionHandler(@"Error: start called while already scanning", nil);
@@ -75,40 +74,39 @@
     // } else if(!isWhereAmIScan) {
     //     // get location at time of survey start
 
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo type:RadarLogTypeSDKCall message:@"calling RadarLocationManager getLocationWithDesiredAccuracy"];
-    [[RadarLocationManager sharedInstance]
-        getLocationWithDesiredAccuracy:RadarTrackingOptionsDesiredAccuracyMedium
-                     completionHandler:^(RadarStatus status, CLLocation *_Nullable location, BOOL stopped) {
-                         if (status != RadarStatusSuccess) {
-                             return;
-                         }
+    NSLog(@"calling Radar getLocationWithDesiredAccuracy");
+    [Radar getLocationWithDesiredAccuracy:RadarTrackingOptionsDesiredAccuracyMedium
+                        completionHandler:^(RadarStatus status, CLLocation *_Nullable location, BOOL stopped) {
+                            if (status != RadarStatusSuccess) {
+                                return;
+                            }
 
-                         [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"location: %f, %f", location.coordinate.latitude, location.coordinate.longitude]];
-                         [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"%@", location]];
+                            NSLog(@"location: %f, %f", location.coordinate.latitude, location.coordinate.longitude);
+                            NSLog(@"%@", location);
 
-                         // print the location being set
-                         [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"setting locationAtTimeOfSurveyStart TO: %@", location]];
-                         self.locationAtTimeOfSurveyStart = location;
+                            // print the location being set
+                            NSLog(@"setting locationAtTimeOfSurveyStart TO: %@", location);
+                            self.locationAtTimeOfSurveyStart = location;
 
-                         [self kickOffMotionAndBluetooth:surveyLengthSeconds];
-    }];
+                            [self kickOffMotionAndBluetooth:surveyLengthSeconds];
+                        }];
 
     // }
 }
 
 - (void)kickOffMotionAndBluetooth:(int)surveyLengthSeconds {
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"kicking off CMMotionManager"];
+    NSLog(@"kicking off CMMotionManager");
     self.motionManager = [[CMMotionManager alloc] init];
     [self.motionManager startMagnetometerUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMMagnetometerData *magnetometerData, NSError *error) {
         if (error) {
-            [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelError message:[NSString stringWithFormat:@"startMagnetometerUpdatesToQueue error: %@", error]];
+            NSLog(@"startMagnetometerUpdatesToQueue error: %@", error);
         } else {
             self.lastMagnetometerData = magnetometerData;
         }
     }];
 
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"kicking off CBCentralManager"];
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"time: %f", [[NSDate date] timeIntervalSince1970]]];
+    NSLog(@"kicking off CBCentralManager");
+    NSLog(@"time: %f", [[NSDate date] timeIntervalSince1970]);
 
     // kick off the survey by init'ing the corebluetooth manager
     self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
@@ -116,13 +114,13 @@
 }
 
 - (void)startScanning {
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"startScanning called --- calling scanForPeripheralsWithServices"];
+    NSLog(@"startScanning called --- calling scanForPeripheralsWithServices");
     [self.centralManager scanForPeripheralsWithServices:nil options:@{CBCentralManagerScanOptionAllowDuplicatesKey: @YES}];
 }
 
 - (void)stopScanning {
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"stopScanning called"];
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"time: %f", [[NSDate date] timeIntervalSince1970]]];
+    NSLog(@"stopScanning called");
+    NSLog(@"time: %f", [[NSDate date] timeIntervalSince1970]);
 
     [self.centralManager stopScan];
 
@@ -132,26 +130,30 @@
     // join all self.bluetoothReadings with newlines and POST to server
     NSString *payload = [self.bluetoothReadings componentsJoinedByString:@"\n"];
 
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"[RadarUtils isSimulator]: %d", [RadarUtils isSimulator]]];
+    BOOL isSimulator = NO;
+#if TARGET_IPHONE_SIMULATOR
+    isSimulator = YES;
+#endif
+    NSLog(@"isSimulator: %d", isSimulator);
 
-    if ([RadarUtils isSimulator]) {
+    if (isSimulator) {
         payload = @"?time=1716583686.556668&label=FAKE-SIMULATOR-FAKE-SIMULATOR-geofence.description:RDR-T__geofence._id:664dfae54dfd1b59d5aff925&peripheral.identifier=C9AC95A0-D6B5-D57F-014B-0FDD11D51E7E&peripheral.name=(no%20name)&rssi=-88&manufacturerId=&scanId=4FCAE527-99B8-4BB8-81F3-75D41C2323B1&serviceUUIDs=(no%20services)&location.coordinate.latitude=40.734173&location.coordinate.longitude=-73.990878&location.horizontalAccuracy=21.840953&location.verticalAccuracy=24.163757&location.altitude=41.885521&location.ellipsoidalAltitude=8.913273&location.timestamp=-0.000000&location.floor=0&sdkVersion=3.9.12&deviceType=iOS&deviceMake=Apple&deviceModel=iPhone14,2&deviceOS=17.4.1&magnetometer.field.x=-35.025116&magnetometer.field.y=-112.979324&magnetometer.field.z=-240.243347&magnetometer.timestamp=65614.479717&magnetometer.field.magnitude=267.783406\n?time=1716583686.559950&label=FAKE-SIMULATOR-FAKE-SIMULATOR-geofence.description:RDR-T__geofence._id:664dfae54dfd1b59d5aff925&peripheral.identifier=540803B9-86A3-CF2E-2A4B-1B23C6DB0214&peripheral.name=%5BTV%5D%20Samsung%209%20Series%20(86)&rssi=-71&manufacturerId=7500&scanId=4FCAE527-99B8-4BB8-81F3-75D41C2323B1&serviceUUIDs=(no%20services)&location.coordinate.latitude=40.734173&location.coordinate.longitude=-73.990878&location.horizontalAccuracy=21.840953&location.verticalAccuracy=24.163757&location.altitude=41.885521&location.ellipsoidalAltitude=8.913273&location.timestamp=-0.000000&location.floor=0&sdkVersion=3.9.12&deviceType=iOS&deviceMake=Apple&deviceModel=iPhone14,2&deviceOS=17.4.1&magnetometer.field.x=-35.025116&magnetometer.field.y=-112.979324&magnetometer.field.z=-240.243347&magnetometer.timestamp=65614.479717&magnetometer.field.magnitude=267.783406\n?time=1716583686.560602&label=FAKE-SIMULATOR-FAKE-SIMULATOR-geofence.description:RDR-T__geofence._id:664dfae54dfd1b59d5aff925&peripheral.identifier=540803B9-86A3-CF2E-2A4B-1B23C6DB0214&peripheral.name=%5BTV%5D%20Samsung%209%20Series%20(86)&rssi=-71&manufacturerId=7500&scanId=4FCAE527-99B8-4BB8-81F3-75D41C2323B1&serviceUUIDs=(no%20services)&location.coordinate.latitude=40.734173&location.coordinate.longitude=-73.990878&location.horizontalAccuracy=21.840953&location.verticalAccuracy=24.163757&location.altitude=41.885521&location.ellipsoidalAltitude=8.913273&location.timestamp=-0.000000&location.floor=0&sdkVersion=3.9.12&deviceType=iOS&deviceMake=Apple&deviceModel=iPhone14,2&deviceOS=17.4.1&magnetometer.field.x=-35.025116&magnetometer.field.y=-112.979324&magnetometer.field.z=-240.243347&magnetometer.timestamp=65614.479717&magnetometer.field.magnitude=267.783406\n?time=1716583686.563503&label=FAKE-SIMULATOR-FAKE-SIMULATOR-geofence.description:RDR-T__geofence._id:664dfae54dfd1b59d5aff925&peripheral.identifier=E25D13C2-08A0-9598-327A-CC4B2F782E53&peripheral.name=(no%20name)&rssi=-70&manufacturerId=&scanId=4FCAE527-99B8-4BB8-81F3-75D41C2323B1&serviceUUIDs=(no%20services)&location.coordinate.latitude=40.734173&location.coordinate.longitude=-73.990878&location.horizontalAccuracy=21.840953&location.verticalAccuracy=24.163757&location.altitude=41.885521&location.ellipsoidalAltitude=8.913273&location.timestamp=-0.000000&location.floor=0&sdkVersion=3.9.12&deviceType=iOS&deviceMake=Apple&deviceModel=iPhone14,2&deviceOS=17.4.1&magnetometer.field.x=-35.025116&magnetometer.field.y=-112.979324&magnetometer.field.z=-240.243347&magnetometer.timestamp=65614.479717&magnetometer.field.magnitude=267.783406\n?time=1716583686.568083&label=FAKE-SIMULATOR-FAKE-SIMULATOR-geofence.description:RDR-T__geofence._id:664dfae54dfd1b59d5aff925&peripheral.identifier=0B9BD1B0-D482-31D2-B723-882E5AD9FCEA&peripheral.name=(no%20name)&rssi=-88&manufacturerId=0600&scanId=4FCAE527-99B8-4BB8-81F3-75D41C2323B1&serviceUUIDs=(no%20services)&location.coordinate.latitude=40.734173&location.coordinate.longitude=-73.990878&location.horizontalAccuracy=21.840953&location.verticalAccuracy=24.163757&location.altitude=41.885521&location.ellipsoidalAltitude=8.913273&location.timestamp=-0.000000&location.floor=0&sdkVersion=3.9.12&deviceType=iOS&deviceMake=Apple&deviceModel=iPhone14,2&deviceOS=17.4.1&magnetometer.field.x=-35.025116&magnetometer.field.y=-112.979324&magnetometer.field.z=-240.243347&magnetometer.timestamp=65614.479717&magnetometer.field.magnitude=267.783406\n?time=1716583686.572948&label=FAKE-SIMULATOR-FAKE-SIMULATOR-geofence.description:RDR-T__geofence._id:664dfae54dfd1b59d5aff925&peripheral.identifier=D48D165B-FE69-C705-6B14-4000822C5EC7&peripheral.name=(no%20name)&rssi=-78&manufacturerId=&scanId=4FCAE527-99B8-4BB8-81F3-75D41C2323B1&serviceUUIDs=(no%20services)&location.coordinate.latitude=40.734173&location.coordinate.longitude=-73.990878&location.horizontalAccuracy=21.840953&location.verticalAccuracy=24.163757&location.altitude=41.885521&location.ellipsoidalAltitude=8.913273&location.timestamp=-0.000000&location.floor=0&sdkVersion=3.9.12&deviceType=iOS&deviceMake=Apple&deviceModel=iPhone14,2&deviceOS=17.4.1&magnetometer.field.x=-35.025116&magnetometer.field.y=-112.979324&magnetometer.field.z=-240.243347&magnetometer.timestamp=65614.479717&magnetometer.field.magnitude=267.783406\n?time=1716583686.573204&label=FAKE-SIMULATOR-FAKE-SIMULATOR-geofence.description:RDR-T__geofence._id:664dfae54dfd1b59d5aff925&peripheral.identifier=D48D165B-FE69-C705-6B14-4000822C5EC7&peripheral.name=(no%20name)&rssi=-78&manufacturerId=&scanId=4FCAE527-99B8-4BB8-81F3-75D41C2323B1&serviceUUIDs=(no%20services)&location.coordinate.latitude=40.734173&location.coordinate.longitude=-73.990878&location.horizontalAccuracy=21.840953&location.verticalAccuracy=24.163757&location.altitude=41.885521&location.ellipsoidalAltitude=8.913273&location.timestamp=-0.000000&location.floor=0&sdkVersion=3.9.12&deviceType=iOS&deviceMake=Apple&deviceModel=iPhone14,2&deviceOS=17.4.1&magnetometer.field.x=-35.025116&magnetometer.field.y=-112.979324&magnetometer.field.z=-240.243347&magnetometer.timestamp=65614.479717&magnetometer.field.magnitude=267.783406\n?time=1716583686.573577&label=FAKE-SIMULATOR-FAKE-SIMULATOR-geofence.description:RDR-T__geofence._id:664dfae54dfd1b59d5aff925&peripheral.identifier=E6FD4D8E-6C80-EACC-6B93-9DDB8D9AAAA5&peripheral.name=(no%20name)&rssi=-73&manufacturerId=&scanId=4FCAE527-99B8-4BB8-81F3-75D41C2323B1&serviceUUIDs=(no%20services)&location.coordinate.latitude=40.734173&location.coordinate.longitude=-73.990878&location.horizontalAccuracy=21.840953&location.verticalAccuracy=24.163757&location.altitude=41.885521&location.ellipsoidalAltitude=8.913273&location.timestamp=-0.000000&location.floor=0&sdkVersion=3.9.12&deviceType=iOS&deviceMake=Apple&deviceModel=iPhone14,2&deviceOS=17.4.1&magnetometer.field.x=-35.025116&magnetometer.field.y=-112.979324&magnetometer.field.z=-240.243347&magnetometer.timestamp=65614.479717&magnetometer.field.magnitude=267.783406\n?time=1716583686.590971&label=FAKE-SIMULATOR-FAKE-SIMULATOR-geofence.description:RDR-T__geofence._id:664dfae54dfd1b59d5aff925&peripheral.identifier=571CE11A-F8BE-43AC-460C-C1A9FA3F0406&peripheral.name=(no%20name)&rssi=-64&manufacturerId=&scanId=4FCAE527-99B8-4BB8-81F3-75D41C2323B1&serviceUUIDs=(no%20services)&location.coordinate.latitude=40.734173&location.coordinate.longitude=-73.990878&location.horizontalAccuracy=21.840953&location.verticalAccuracy=24.163757&location.altitude=41.885521&location.ellipsoidalAltitude=8.913273&location.timestamp=-0.000000&location.floor=0&sdkVersion=3.9.12&deviceType=iOS&deviceMake=Apple&deviceModel=iPhone14,2&deviceOS=17.4.1&magnetometer.field.x=-29.425323&magnetometer.field.y=-112.600601&magnetometer.field.z=-246.101929&magnetometer.timestamp=65614.520519&magnetometer.field.magnitude=272.233180\n";
     }
 
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"payload length: %lu", (unsigned long)[payload length]]];
+    NSLog(@"payload length: %lu", (unsigned long)[payload length]);
 
     // compress payload and base64 encode it
     // compress payload
     NSData *compressedData = [payload dataUsingEncoding:NSUTF8StringEncoding];
     NSData *compressedDataGzipped = [compressedData gzippedData];
     // print length of compressed payload
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"compressedDataGzipped length: %lu", (unsigned long)[compressedDataGzipped length]]];
+    NSLog(@"compressedDataGzipped length: %lu", (unsigned long)[compressedDataGzipped length]);
 
     // base64 encode
     NSString *compressedDataGzippedBase64 = [compressedDataGzipped base64EncodedStringWithOptions:0];
      
     // print length of base64 encoded payload
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"compressedDataGzippedBase64 length: %lu", (unsigned long)[compressedDataGzippedBase64 length]]];
+    NSLog(@"compressedDataGzippedBase64 length: %lu", (unsigned long)[compressedDataGzippedBase64 length]);
 
     // NEW WORKFLOW:
     // Instead of posting directly to server, we return the data to the caller
@@ -205,7 +207,7 @@
     // }
     */
 
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"calling removeAllObjects, clearing scanId, etc."];
+    NSLog(@"calling removeAllObjects, clearing scanId, etc.");
 
     // removeAllObjects from bluetooth readings i.e. clear array
     // locationAtTimeOfSurveyStart will be reset after completionhandler sends back the value
@@ -214,36 +216,42 @@
     self.lastMagnetometerData = nil;
     self.locationAtTimeOfSurveyStart = nil;
 
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"stopScanning end"];
+    NSLog(@"stopScanning end");
 
     self.isScanning = NO;
+}
+
+- (NSString *)deviceModel {
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
 }
 
 - (void)centralManagerDidUpdateState:(CBCentralManager *)central {
     switch (central.state) {
         case CBManagerStatePoweredOff:
-            [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"CBCentralManager: Is Powered Off."];
+            NSLog(@"CBCentralManager: Is Powered Off.");
             break;
         case CBManagerStatePoweredOn:
-            [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"CBCentralManager: Is Powered On."];
+            NSLog(@"CBCentralManager: Is Powered On.");
             // print time
-            [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:[NSString stringWithFormat:@"time: %f", [[NSDate date] timeIntervalSince1970]]];
+            NSLog(@"time: %f", [[NSDate date] timeIntervalSince1970]);
             [self startScanning];
             break;
         case CBManagerStateUnsupported:
-            [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"CBCentralManager: Is Unsupported."];
+            NSLog(@"CBCentralManager: Is Unsupported.");
             break;
         case CBManagerStateUnauthorized:
-            [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"CBCentralManager: Is Unauthorized."];
+            NSLog(@"CBCentralManager: Is Unauthorized.");
             break;
         case CBManagerStateUnknown:
-            [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"CBCentralManager: Unknown"];
+            NSLog(@"CBCentralManager: Unknown");
             break;
         case CBManagerStateResetting:
-            [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"CBCentralManager: Resetting"];
+            NSLog(@"CBCentralManager: Resetting");
             break;
         default:
-            [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"CBCentralManager: Error"];
+            NSLog(@"CBCentralManager: Error");
             break;
     }
 }
@@ -289,11 +297,11 @@
                 @(self.locationAtTimeOfSurveyStart.floor.level) : @0
         ]],
         
-        [NSURLQueryItem queryItemWithName:@"sdkVersion" value:[RadarUtils sdkVersion]],
-        [NSURLQueryItem queryItemWithName:@"deviceType" value:[RadarUtils deviceType]],
-        [NSURLQueryItem queryItemWithName:@"deviceMake" value:[RadarUtils deviceMake]],
-        [NSURLQueryItem queryItemWithName:@"deviceModel" value:[RadarUtils deviceModel]],
-        [NSURLQueryItem queryItemWithName:@"deviceOS" value:[RadarUtils deviceOS]],
+        [NSURLQueryItem queryItemWithName:@"sdkVersion" value:[Radar sdkVersion]],
+        [NSURLQueryItem queryItemWithName:@"deviceType" value:@"iOS"],
+        [NSURLQueryItem queryItemWithName:@"deviceMake" value:@"Apple"],
+        [NSURLQueryItem queryItemWithName:@"deviceModel" value:[self deviceModel]],
+        [NSURLQueryItem queryItemWithName:@"deviceOS" value:[[NSProcessInfo processInfo] operatingSystemVersionString]],
 
         // inject x, y, z from self.lastMagnetometerData
         // and add sqrt(pow(magnet.field.x, 2) + pow(magnet.field.y, 2) + pow(magnet.field.z, 2)) as well
@@ -320,7 +328,7 @@
 }
 
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
-    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelInfo message:@"centralManager didConnectPeripheral, calling stopScanning"];
+    NSLog(@"centralManager didConnectPeripheral, calling stopScanning");
     [self stopScanning];
 }
 
