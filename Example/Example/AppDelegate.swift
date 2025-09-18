13a44cb55aa0aebe4ef113cfa844d70d27b074e4
ShiCheng Lu <shicheng.lu@radar.com>
2025-09-18 14:25:43 -0400

    cleanup

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -32,11 +32,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIWindowSceneDelegate, UN
         // Replace with a valid test publishable key
         let radarInitializeOptions = RadarInitializeOptions()
         
-        UserDefaults.standard.set("https://api.radar-staging.com", forKey: "radar-host")
         // Uncomment to enable automatic setup for notification conversions or deep linking
         //radarInitializeOptions.autoLogNotificationConversions = true
         //radarInitializeOptions.autoHandleNotificationDeepLinks = true
-        Radar.initialize(publishableKey: "prj_test_pk_", options: radarInitializeOptions )
+        Radar.initialize(publishableKey: "prj_test_pk_0000000000000000000000000000000000000000", options: radarInitializeOptions )
         Radar.setUserId("testUserId")
         Radar.setMetadata([ "foo": "bar" ])
         Radar.setDelegate(self)
