fbea8d99a4d9b34218e4166a52e10093a963f87e
ShiCheng Lu <shicheng.lu@radar.com>
2025-10-21 11:41:48 -0400

    split views

---
+++ Example/Example/LogsView.swift
@@ -0,0 +1,50 @@
+//
+//  LogsView.swift
+//  Example
+//
+//  Copyright © 2025 Radar Labs, Inc. All rights reserved.
+//
+
+import SwiftUI
+import RadarSDK
+
+struct LogsView: View {
+    @StateObject var radarDelegateState = RadarDelegateState()
+    let radarDelegate = MyRadarDelegate()
+
+    init() {
+        Radar.setDelegate(radarDelegate)
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Logs")
+                Button("clear") {
+                    radarDelegateState.logs.removeAll()
+                }
+            }
+            List(radarDelegateState.logs, id:\.0) { item in
+                return Text("\(item.1)")
+            }
+
+            HStack {
+                Text("Events")
+                Button("clear") {
+                    radarDelegateState.logs.removeAll()
+                }
+            }
+            List(radarDelegateState.events, id:\.self) { item in
+                let type = RadarEvent.string(for: item.type) ?? "unknown-type"
+                var description = ""
+                if let geofence = item.geofence {
+                    description = geofence.externalId ?? ""
+                }
+                return Text("\(type): \(description)")
+            }
+
+        }.onAppear {
+            radarDelegate.state = radarDelegateState
+        }
+    }
+}
