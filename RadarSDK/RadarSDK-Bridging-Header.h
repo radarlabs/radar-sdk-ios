e8d6c2557a5b87cf6e27124f4a2d0252fcc658c9
ShiCheng Lu <shicheng.lu@radar.com>
2025-12-19 10:03:40 -0500

    add bridging header

---
+++ RadarSDK/RadarSDK-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  RadarSDK-Bridging-Header.h
+//  RadarSDK
+//
+//  Copyright © 2025 Radar Labs, Inc. All rights reserved.
+//
+
+#import "RadarDelegate.h"
+#imoprt "RadarInAppMessageDelegate.h"
+#import "RadarSwiftBridge.h"
