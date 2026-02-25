51d2922f6ce5a555879a29a84b0da26c873d47f5
Liam Meier <liam.meier@radar.com>
2026-02-25 16:20:36 -0500

    Persist altitude adjustments clientside (#530)
    * store altitude adjustments on the client
    
    * add tests
    
    * clear stale altitude updates
    
    * fix getter and setter in radar state
    
    * use object methods instead of value methods
    
    ---------
    
    Co-authored-by: Alan <alan.charles@radar.com>

--- RadarSDK/RadarAPIClient.m
+++ RadarSDK/RadarAPIClient.m
@@ -415,6 +415,12 @@
         params[@"locationMetadata"] = locationMetadata;
     }
 
+    // Include stored altitudeAdjustments from previous track responses
+    NSArray<NSDictionary *> *altitudeAdjustments = [RadarState altitudeAdjustments];
+    if (altitudeAdjustments && altitudeAdjustments.count > 0) {
+        params[@"altitudeAdjustments"] = altitudeAdjustments;
+    }
+
     if (anonymous) {
         [[RadarAPIClient sharedInstance] getConfigForUsage:@"track"
                                                 verified:verified
@@ -531,6 +537,15 @@
                                 NSMutableDictionary *mutableUserObj = [userObj mutableCopy];
                                 mutableUserObj[@"metadata"] = locationMetadata;
                                 userObj = mutableUserObj;
+                                
+                                // Extract and store altitudeAdjustments from user object
+                                id altitudeAdjustmentsObj = mutableUserObj[@"altitudeAdjustments"];
+                                if ([altitudeAdjustmentsObj isKindOfClass:[NSArray class]]) {
+                                    [RadarState setAltitudeAdjustments:(NSArray *)altitudeAdjustmentsObj];
+                                    [[RadarLogger sharedInstance] logWithLevel:RadarLogLevelDebug message:[NSString stringWithFormat:@"Stored %lu altitude adjustments from track response", (unsigned long)[(NSArray *)altitudeAdjustmentsObj count]]];
+                                } else {
+                                    [RadarState setAltitudeAdjustments:nil];
+                                }
                             }
                             id nearbyGeofencesObj = res[@"nearbyGeofences"];
                             id inAppMessagesObj = res[@"inAppMessages"];
