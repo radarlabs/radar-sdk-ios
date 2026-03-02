02842984f485164b391bb555b64339f4221cd4ec
Alan <alan.charles@radar.com>
2026-03-02 12:28:58 -0700

    add performRequest:isRetry:

--- RadarSDK/RadarAPIHelper.m
+++ RadarSDK/RadarAPIHelper.m
@@ -50,204 +50,204 @@
                logPayload:(BOOL)logPayload
           extendedTimeout:(BOOL)extendedTimeout
         completionHandler:(RadarAPICompletionHandler)completionHandler {
-    [self requestWithMethod:method
-                        url:url
-                    headers:headers
-                     params:params
-                      sleep:sleep
-                 logPayload:logPayload
-            extendedTimeout:extendedTimeout
-                    isRetry:NO
-          completionHandler:completionHandler];
-}
-
-- (void)requestWithMethod:(NSString *)method
-                      url:(NSString *)url
-                  headers:(NSDictionary *)headers
-                   params:(NSDictionary *)params
-                    sleep:(BOOL)sleep
-               logPayload:(BOOL)logPayload
-          extendedTimeout:(BOOL)extendedTimeout
-                  isRetry:(BOOL)isRetry
-        completionHandler:(RadarAPICompletionHandler)completionHandler {
     dispatch_async(self.queue, ^{
         if (sleep) {
             dispatch_semaphore_wait(self.semaphore, DISPATCH_TIME_FOREVER);
         }
+        [self performRequestWithMethod:method
+                                   url:url
+                               headers:headers
+                                params:params
+                                 sleep:sleep
+                            logPayload:logPayload
+                       extendedTimeout:extendedTimeout
+                               isRetry:NO
+                     completionHandler:completionHandler];
+    });
+}
 
-        NSMutableURLRequest *req = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
-        req.HTTPMethod = method;
+- (void)performRequestWithMethod:(NSString *)method
+                             url:(NSString *)url
+                         headers:(NSDictionary *)headers
+                          params:(NSDictionary *)params
+                           sleep:(BOOL)sleep
+                      logPayload:(BOOL)logPayload
+                 extendedTimeout:(BOOL)extendedTimeout
+                         isRetry:(BOOL)isRetry
+               completionHandler:(RadarAPICompletionHandler)completionHandler {
+    NSMutableURLRequest * req = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
+    req.HTTPMethod = method;
 
-        NSString * paramJsonStr = [RadarUtils dictionaryToJson:params];
-        NSString * headersJsonStr = [RadarUtils dictionaryToJson:headers];
+    NSString * paramJsonStr = [RadarUtils dictionaryToJson:params];
+    NSString * headersJsonStr = [RadarUtils dictionaryToJson:headers];
 
-        if (logPayload) {
-            [[RadarLogger sharedInstance]
-                logWithLevel:RadarLogLevelDebug
-                     message:[NSString stringWithFormat:@"📍 Radar API request | method = %@; url = %@; headers = %@; params = %@", method, url, headersJsonStr, paramJsonStr]];
-        } else {
-            [[RadarLogger sharedInstance]
-                logWithLevel:RadarLogLevelDebug
-                     message:[NSString stringWithFormat:@"📍 Radar API request | method = %@; url = %@; headers = %@", method, url, headersJsonStr]];
+    if (logPayload) {
+        [[RadarLogger sharedInstance]
+            logWithLevel:RadarLogLevelDebug
+                 message:[NSString stringWithFormat:@"📍 Radar API request | method = %@; url = %@; headers = %@; params = %@", method, url, headersJsonStr, paramJsonStr]];
+    } else {
+        [[RadarLogger sharedInstance]
+            logWithLevel:RadarLogLevelDebug
+                 message:[NSString stringWithFormat:@"📍 Radar API request | method = %@; url = %@; headers = %@", method, url, headersJsonStr]];
+    }
+
+    @try {
+        if (headers) {
+            for (NSString * key in headers) {
+                [req addValue:[headers valueForKey:key] forHTTPHeaderField:key];
+            }
         }
 
-        @try {
-            if (headers) {
-                for (NSString *key in headers) {
-                    [req addValue:[headers valueForKey:key] forHTTPHeaderField:key];
+        if (params) {
+            NSNumber * prevUpdatedAtMsDiff = params[@"updatedAtMsDiff"];
+            NSArray * replays = params[@"replays"];
+            if (prevUpdatedAtMsDiff || replays) {
+                NSMutableDictionary *requestParams = [params mutableCopy];
+                long nowMs = (long)([NSDate date].timeIntervalSince1970 * 1000);
+                NSNumber *locationMs = params[@"locationMs"];
+
+                if (locationMs != nil && prevUpdatedAtMsDiff != nil) {
+                    long updatedAtMsDiff = nowMs - [locationMs longValue];
+                    requestParams[@"updatedAtMsDiff"] = @(updatedAtMsDiff);
                 }
-            }
 
-            if (params) {
-                NSNumber *prevUpdatedAtMsDiff = params[@"updatedAtMsDiff"];
-                NSArray *replays = params[@"replays"];
-                if (prevUpdatedAtMsDiff || replays) {
-                    NSMutableDictionary *requestParams = [params mutableCopy];
-                    long nowMs = (long)([NSDate date].timeIntervalSince1970 * 1000);
-                    NSNumber *locationMs = params[@"locationMs"];
-
-                    if (locationMs != nil && prevUpdatedAtMsDiff != nil) {
-                        long updatedAtMsDiff = nowMs - [locationMs longValue];
-                        requestParams[@"updatedAtMsDiff"] = @(updatedAtMsDiff);
-                    }
-
-                    if (replays) {
-                        NSMutableArray *updatedReplays = [NSMutableArray arrayWithCapacity:replays.count];
-                        for (NSDictionary *replay in replays) {
-                            NSMutableDictionary *updatedReplay = [replay mutableCopy];
-                            NSNumber *replayLocationMs = replay[@"locationMs"];
-                            if (replayLocationMs != nil) {
-                                long replayUpdatedAtMsDiff = nowMs - [replayLocationMs longValue];
-                                updatedReplay[@"updatedAtMsDiff"] = @(replayUpdatedAtMsDiff);
-                            }
-                            [updatedReplays addObject:updatedReplay];
+                if (replays) {
+                    NSMutableArray *updatedReplays = [NSMutableArray arrayWithCapacity:replays.count];
+                    for (NSDictionary *replay in replays) {
+                        NSMutableDictionary *updatedReplay = [replay mutableCopy];
+                        NSNumber *replayLocationMs = replay[@"locationMs"];
+                        if (replayLocationMs != nil) {
+                            long replayUpdatedAtMsDiff = nowMs - [replayLocationMs longValue];
+                            updatedReplay[@"updatedAtMsDiff"] = @(replayUpdatedAtMsDiff);
                         }
-                        requestParams[@"replays"] = updatedReplays;
+                        [updatedReplays addObject:updatedReplay];
                     }
-
-                    [req setHTTPBody:[NSJSONSerialization dataWithJSONObject:requestParams options:0 error:NULL]];
-                } else {
-                    [req setHTTPBody:[NSJSONSerialization dataWithJSONObject:params options:0 error:NULL]];
+                    requestParams[@"replays"] = updatedReplays;
                 }
+
+                [req setHTTPBody:[NSJSONSerialization dataWithJSONObject:requestParams options:0 error:NULL]];
+            } else {
+                [req setHTTPBody:[NSJSONSerialization dataWithJSONObject:params options:0 error:NULL]];
             }
+        }
 
-            NSURLSession *session = extendedTimeout ? self.extendedTimeoutSession : self.standardSession;
-            NSDate *requestStart = [NSDate date];
+        NSURLSession * session = extendedTimeout ? self.extendedTimeoutSession : self.standardSession;
+        NSDate * requestStart = [NSDate date];
 
-            void (^dataTaskCompletionHandler)(NSData *, NSURLResponse *, NSError *) = ^(NSData *data, NSURLResponse *response, NSError *error) {
-                NSTimeInterval latency = [requestStart timeIntervalSinceNow] * -1;
+        void (^dataTaskCompletionHandler)(NSData *, NSURLResponse *, NSError *) = ^(NSData *data, NSURLResponse *response, NSError *error) {
+            NSTimeInterval latency = [requestStart timeIntervalSinceNow] * -1;
 
-                if (error) {
-                    BOOL isLostConnection = (error.domain == NSURLErrorDomain &&
-                                             error.code == NSURLErrorNetworkConnectionLost);
+            if (error) {
+                BOOL isLostConnection = ([error.domain isEqualToString:NSURLErrorDomain] &&
+                error.code == NSURLErrorNetworkConnectionLost);
 
-                    if (isLostConnection && !isRetry) {
-                        [[RadarLogger sharedInstance]
-                            logWithLevel:RadarLogLevelDebug
-                                 message:[NSString stringWithFormat:@"📍 Radar API retrying after lost connection | url = %@", url]];
-                        // Must signal before dispatching retry to avoid semaphore deadlock
-                        if (sleep) {
-                            dispatch_semaphore_signal(self.semaphore);
-                        }
-                        [self requestWithMethod:method
-                                            url:url
-                                        headers:headers
-                                         params:params
-                                          sleep:sleep
-                                     logPayload:logPayload
-                                extendedTimeout:extendedTimeout
-                                        isRetry:YES
-                              completionHandler:completionHandler];
-                        return;
-                    }
-
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        [[RadarLogger sharedInstance]
-                            logWithLevel:RadarLogLevelError
-                                    type:RadarLogTypeSDKError
-                                 message:[NSString stringWithFormat:@"Received network error | error = %@", error]];
-                        completionHandler(RadarStatusErrorNetwork, nil);
-                    });
-
-                    if (sleep) {
-                        [NSThread sleepForTimeInterval:1];
-                        dispatch_semaphore_signal(self.semaphore);
-                    }
-
+                if (isLostConnection && !isRetry) {
+                    [[RadarLogger sharedInstance]
+                        logWithLevel:RadarLogLevelDebug
+                             message:[NSString stringWithFormat:@"📍 Radar API retrying after lost connection | url = %@", url]];
+                    // Call inline so the semaphore stays held
+                    // and the retry runs before any other queued sleep=YES requests.
+                    [self performRequestWithMethod:method
+                                              url:url
+                                          headers:headers
+                                           params:params
+                                            sleep:sleep
+                                       logPayload:logPayload
+                                  extendedTimeout:extendedTimeout
+                                          isRetry:YES
+                                completionHandler:completionHandler];
                     return;
-                }
-
-                NSError *deserializationError = nil;
-                id resObj = [NSJSONSerialization JSONObjectWithData:data options:0 error:&deserializationError];
-                if (deserializationError || ![resObj isKindOfClass:[NSDictionary class]]) {
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        completionHandler(RadarStatusErrorServer, nil);
-                    });
-
-                    if (sleep) {
-                        [NSThread sleepForTimeInterval:1];
-                        dispatch_semaphore_signal(self.semaphore);
-                    }
-
-                    return;
-                }
-
-                NSDictionary *res;
-                RadarStatus status = RadarStatusErrorUnknown;
-
-                if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
-                    NSInteger statusCode = ((NSHTTPURLResponse *)response).statusCode;
-                    if (statusCode >= 200 && statusCode < 400) {
-                        status = RadarStatusSuccess;
-                    } else if (statusCode == 400) {
-                        status = RadarStatusErrorBadRequest;
-                    } else if (statusCode == 401) {
-                        status = RadarStatusErrorUnauthorized;
-                    } else if (statusCode == 402) {
-                        status = RadarStatusErrorPaymentRequired;
-                    } else if (statusCode == 403) {
-                        status = RadarStatusErrorForbidden;
-                    } else if (statusCode == 404) {
-                        status = RadarStatusErrorNotFound;
-                    } else if (statusCode == 429) {
-                        status = RadarStatusErrorRateLimit;
-                    } else if (statusCode >= 500 && statusCode <= 599) {
-                        status = RadarStatusErrorServer;
-                    }
-
-                    res = (NSDictionary *)resObj;
-                    NSString * resJsonStr = [RadarUtils dictionaryToJson:res];
-
-                    if (params && [params objectForKey:@"replays"]) {
-                        NSArray *replays = [params objectForKey:@"replays"];
-                        [[RadarLogger sharedInstance]
-                            logWithLevel:RadarLogLevelDebug
-                                 message:[NSString stringWithFormat:@"📍 Radar API response | method = %@; url = %@; statusCode = %ld; latency = %f; replays = %lu; res = %@",
-                                                                    method, url, (long)statusCode, latency, (unsigned long)replays.count, resJsonStr]];
-                    } else {
-                        [[RadarLogger sharedInstance]
-                            logWithLevel:RadarLogLevelDebug
-                                 message:[NSString stringWithFormat:@"📍 Radar API response | method = %@; url = %@; statusCode = %ld; latency = %f; res = %@",
-                                                                    method, url, (long)statusCode, latency, resJsonStr]];
-                    }
                 }
 
                 dispatch_async(dispatch_get_main_queue(), ^{
-                    completionHandler(status, res);
+                    [[RadarLogger sharedInstance]
+                        logWithLevel:RadarLogLevelError
+                                type:RadarLogTypeSDKError
+                             message:[NSString stringWithFormat:@"Received network error | error = %@", error]];
+                    completionHandler(RadarStatusErrorNetwork, nil);
                 });
 
                 if (sleep) {
                     [NSThread sleepForTimeInterval:1];
                     dispatch_semaphore_signal(self.semaphore);
                 }
-            };
 
-            NSURLSessionDataTask *task = [session dataTaskWithRequest:req completionHandler:dataTaskCompletionHandler];
-            [task resume];
-        } @catch (NSException *exception) {
-            return completionHandler(RadarStatusErrorBadRequest, nil);
+                return;
+            }
+
+            NSError * deserializationError = nil;
+            id resObj = [NSJSONSerialization JSONObjectWithData:data options:0 error:&deserializationError];
+            if (deserializationError || ![resObj isKindOfClass:[NSDictionary class]]) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    completionHandler(RadarStatusErrorServer, nil);
+                });
+
+                if (sleep) {
+                    [NSThread sleepForTimeInterval:1];
+                    dispatch_semaphore_signal(self.semaphore);
+                }
+
+                return;
+            }
+
+            NSDictionary * res;
+            RadarStatus status = RadarStatusErrorUnknown;
+
+            if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+                NSInteger statusCode = ((NSHTTPURLResponse *)response).statusCode;
+                if (statusCode >= 200 && statusCode < 400) {
+                    status = RadarStatusSuccess;
+                } else if (statusCode == 400) {
+                    status = RadarStatusErrorBadRequest;
+                } else if (statusCode == 401) {
+                    status = RadarStatusErrorUnauthorized;
+                } else if (statusCode == 402) {
+                    status = RadarStatusErrorPaymentRequired;
+                } else if (statusCode == 403) {
+                    status = RadarStatusErrorForbidden;
+                } else if (statusCode == 404) {
+                    status = RadarStatusErrorNotFound;
+                } else if (statusCode == 429) {
+                    status = RadarStatusErrorRateLimit;
+                } else if (statusCode >= 500 && statusCode <= 599) {
+                    status = RadarStatusErrorServer;
+                }
+
+                res = (NSDictionary *)resObj;
+                NSString * resJsonStr = [RadarUtils dictionaryToJson:res];
+
+                if (params && [params objectForKey:@"replays"]) {
+                    NSArray *replays = [params objectForKey:@"replays"];
+                    [[RadarLogger sharedInstance]
+                        logWithLevel:RadarLogLevelDebug
+                             message:[NSString stringWithFormat:@"📍 Radar API response | method = %@; url = %@; statusCode = %ld; latency = %f; replays = %lu; res = %@",
+                                                                method, url, (long)statusCode, latency, (unsigned long)replays.count, resJsonStr]];
+                } else {
+                    [[RadarLogger sharedInstance]
+                        logWithLevel:RadarLogLevelDebug
+                             message:[NSString stringWithFormat:@"📍 Radar API response | method = %@; url = %@; statusCode = %ld; latency = %f; res = %@",
+                                                                method, url, (long)statusCode, latency, resJsonStr]];
+                }
+            }
+
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completionHandler(status, res);
+            });
+
+            if (sleep) {
+                [NSThread sleepForTimeInterval:1];
+                dispatch_semaphore_signal(self.semaphore);
+            }
+        };
+
+        NSURLSessionDataTask * task = [session dataTaskWithRequest:req completionHandler:dataTaskCompletionHandler];
+        [task resume];
+    } @catch (NSException *exception) {
+        if (sleep) {
+            dispatch_semaphore_signal(self.semaphore);
         }
-    });
+        return completionHandler(RadarStatusErrorBadRequest, nil);
+    }
 }
 
 @end
