925a8aa526c07be0b54e7437e3063397f5eef2b1
ShiCheng Lu <shicheng.lu@radar.com>
2026-03-25 11:33:43 -0400

    17. Add locale for RadarUtils.isoDateFormatter

--- RadarSDK/RadarUtils.swift
+++ RadarSDK/RadarUtils.swift
@@ -223,6 +223,7 @@ class RadarUtils: NSObject {
     
     static let isoDateFormatter: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         return formatter
