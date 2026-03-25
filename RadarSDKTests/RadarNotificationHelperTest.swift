b50f82dbea5acc91882f3ee8e164e21fcd1d41d9
ShiCheng Lu <shicheng.lu@radar.com>
2026-03-25 10:37:12 -0400

    add test file

---
+++ RadarSDKTests/RadarNotificationHelperTest.swift
@@ -0,0 +1,26 @@
+//
+//  RadarNotificationHelperTest.swift
+//  RadarSDK
+//
+//  Copyright © 2026 Radar Labs, Inc. All rights reserved.
+//
+
+import Foundation
+import Testing
+@testable
+import RadarSDK
+import SwiftUI
+
+
+
+// run tests in series because we want to test UserDefaults.standard, which is a shared instance
+@Suite
+actor RadarNotificationHelperTest {
+    
+    
+    @Test
+    func registering() {
+        
+    }
+    
+}
