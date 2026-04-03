356c6ded93479b0e2f06e83c998eee49c6924a8d
ShiCheng Lu <shicheng.lu@radar.com>
2026-04-03 13:49:02 -0400

    add mockTracking test to CI

--- RadarSDKTests/InAppMessageTest.swift
+++ RadarSDKTests/InAppMessageTest.swift
@@ -7,8 +7,7 @@
 
 import Foundation
 import Testing
-@testable
-import RadarSDK
+@testable import RadarSDK
 import SwiftUI
 
 @available(iOS 13.0, *)
@@ -76,138 +75,143 @@ class MockWindow : UIWindow {
     }
 }
 
-@Suite
-actor InAppMessageTest {
-    @MainActor
-    let message = RadarInAppMessage.fromDictionary([
-        "title": [
-            "text": "This is the title",
-            "color": "#ff0000"
-        ],
-        "body": [
-            "text": "This is a demo message.",
-            "color": "#00ff00"
-        ],
-        "button": [
-            "text": "Buy it",
-            "color": "#0000ff",
-            "backgroundColor": "#EB0083",
-        ],
-        "image": [
-            "url": "https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg",
-            "name": "image.jpeg"
-        ],
-        "metadata": [
-            "campaignId": "1234"
-        ]
-    ])
+@MainActor
+let message = RadarInAppMessage.fromDictionary([
+    "title": [
+        "text": "This is the title",
+        "color": "#ff0000"
+    ],
+    "body": [
+        "text": "This is a demo message.",
+        "color": "#00ff00"
+    ],
+    "button": [
+        "text": "Buy it",
+        "color": "#0000ff",
+        "backgroundColor": "#EB0083",
+    ],
+    "image": [
+        "url": "https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg",
+        "name": "image.jpeg"
+    ],
+    "metadata": [
+        "campaignId": "1234"
+    ]
+])
 
-    @Test("In app message construction")
-    @MainActor
-    func InAppMessageTestConstruction() throws {
-        let message = message as? RadarInAppMessage_Swift
-        
-        #expect(message != nil)
-        #expect(message!.title.text == "This is the title")
-        #expect(message!.title.color == UIColor(red: 1, green: 0, blue: 0, alpha: 1))
-        #expect(message!.body.text == "This is a demo message.")
-        #expect(message!.body.color == UIColor(red: 0, green: 1, blue: 0, alpha: 1))
-        #expect(message!.button?.text == "Buy it")
-        #expect(message!.button?.color == UIColor(red: 0, green: 0, blue: 1, alpha: 1))
-        #expect(message!.button?.backgroundColor == UIColor(red: 0xeb/255, green: 0x00/255, blue: 0x83/255, alpha: 1))
-        #expect(message!.image?.name == "image.jpeg")
-        #expect(message!.image?.url == "https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg")
-        #expect(message!.metadata["campaignId"] as? String == "1234")
-    }
+// These needs to be main actor because only main actor can access the window
+extension RadarSwiftParallelTests {
     
-    @Test("In app message to dictionary")
-    @MainActor
-    func InAppMessageTestToDictionary() throws {
-        let dict = message?.toDictionary()
-        #expect(dict != nil)
+    @Suite
+    struct RadarInAppMessageTests {
         
-        let title = dict!["title"] as? [String: String]
-        #expect(title != nil)
-        #expect(title!["text"] == "This is the title")
-        #expect(title!["color"] == "#ff0000")
+        @Test("In app message construction")
+        @MainActor
+        func InAppMessageTestConstruction() throws {
+            let message = message as? RadarInAppMessage_Swift
+            
+            #expect(message != nil)
+            #expect(message!.title.text == "This is the title")
+            #expect(message!.title.color == UIColor(red: 1, green: 0, blue: 0, alpha: 1))
+            #expect(message!.body.text == "This is a demo message.")
+            #expect(message!.body.color == UIColor(red: 0, green: 1, blue: 0, alpha: 1))
+            #expect(message!.button?.text == "Buy it")
+            #expect(message!.button?.color == UIColor(red: 0, green: 0, blue: 1, alpha: 1))
+            #expect(message!.button?.backgroundColor == UIColor(red: 0xeb/255, green: 0x00/255, blue: 0x83/255, alpha: 1))
+            #expect(message!.image?.name == "image.jpeg")
+            #expect(message!.image?.url == "https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg")
+            #expect(message!.metadata["campaignId"] as? String == "1234")
+        }
         
-        let body = dict!["body"] as? [String: String]
-        #expect(body != nil)
-        #expect(body!["text"] == "This is a demo message.")
-        #expect(body!["color"] == "#00ff00")
+        @Test("In app message to dictionary")
+        @MainActor
+        func InAppMessageTestToDictionary() throws {
+            let dict = message?.toDictionary()
+            #expect(dict != nil)
+            
+            let title = dict!["title"] as? [String: String]
+            #expect(title != nil)
+            #expect(title!["text"] == "This is the title")
+            #expect(title!["color"] == "#ff0000")
+            
+            let body = dict!["body"] as? [String: String]
+            #expect(body != nil)
+            #expect(body!["text"] == "This is a demo message.")
+            #expect(body!["color"] == "#00ff00")
+            
+            let button = dict!["button"] as? [String: String]
+            #expect(button != nil)
+            #expect(button!["text"] == "Buy it")
+            #expect(button!["color"] == "#0000ff")
+            #expect(button!["backgroundColor"] == "#eb0083")
+            
+            let image = dict!["image"] as? [String: String]
+            #expect(image != nil)
+            #expect(image!["url"] == "https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg")
+            #expect(image!["name"] == "image.jpeg")
+            
+            let metadata = dict!["metadata"] as? [String: Any]
+            #expect(metadata != nil)
+            #expect(metadata!["campaignId"] as? String == "1234")
+        }
         
-        let button = dict!["button"] as? [String: String]
-        #expect(button != nil)
-        #expect(button!["text"] == "Buy it")
-        #expect(button!["color"] == "#0000ff")
-        #expect(button!["backgroundColor"] == "#eb0083")
         
-        let image = dict!["image"] as? [String: String]
-        #expect(image != nil)
-        #expect(image!["url"] == "https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg")
-        #expect(image!["name"] == "image.jpeg")
+        @Test("In app message received calls create view")
+        @MainActor
+        @available(iOS 14.0, *)
+        func InAppMessageTestCreateView() async throws {
+            let manager = RadarInAppMessageManager()
+            let mockDelegate = MockRadarInAppMessageDelegate(manager: manager)
+            manager.setDelegate(mockDelegate)
+            let mockWindow = MockWindow()
+            manager.getKeyWindow = { return mockWindow }
+            mockDelegate.showInAppMessage = true
+            
+            manager.onInAppMessageReceived(messages: [message!])
+            
+            await mockWindow.waitForSubviewAddition()
+            
+            #expect(mockDelegate.onNewInAppMessageCounter == 1)
+            #expect(mockDelegate.createInAppMessageViewCounter == 1)
+            
+            // pretend to click on the button
+            mockDelegate.viewOnInAppMessageClicked?()
+            
+            #expect(mockDelegate.onInAppMessageButtonClickedCounter == 1)
+            
+            #expect(manager.view == nil)
+            
+        }
         
-        let metadata = dict!["metadata"] as? [String: Any]
-        #expect(metadata != nil)
-        #expect(metadata!["campaignId"] as? String == "1234")
-    }
-    
-
-    @Test("In app message received calls create view")
-    @MainActor
-    @available(iOS 14.0, *)
-    func InAppMessageTestCreateView() async throws {
-        let manager = RadarInAppMessageManager()
-        let mockDelegate = MockRadarInAppMessageDelegate(manager: manager)
-        manager.setDelegate(mockDelegate)
-        let mockWindow = MockWindow()
-        manager.getKeyWindow = { return mockWindow }
-        mockDelegate.showInAppMessage = true
-
-        manager.onInAppMessageReceived(messages: [message!])
-
-        await mockWindow.waitForSubviewAddition()
-
-        #expect(mockDelegate.onNewInAppMessageCounter == 1)
-        #expect(mockDelegate.createInAppMessageViewCounter == 1)
-
-        // pretend to click on the button
-        mockDelegate.viewOnInAppMessageClicked?()
-
-        #expect(mockDelegate.onInAppMessageButtonClickedCounter == 1)
-
-        #expect(manager.view == nil)
-
-    }
-
-    @Test("if there is already an IAM, don't show another")
-    @MainActor
-    @available(iOS 13.0, *)
-    func InAppMessageViewAlreadyExist() async throws {
-        let manager = RadarInAppMessageManager()
-        let mockDelegate = MockRadarInAppMessageDelegate(manager: manager)
-        manager.setDelegate(mockDelegate)
-        let mockWindow = MockWindow()
-        manager.getKeyWindow = { return mockWindow }
-
-        mockDelegate.showInAppMessage = true
-
-        manager.onInAppMessageReceived(messages: [message!])
-        manager.onInAppMessageReceived(messages: [message!])
-
-        await mockWindow.waitForSubviewAddition()
-
-        #expect(mockDelegate.onNewInAppMessageCounter == 2)
-        // 2 views could be created since creating the view is async
-        #expect(mockDelegate.createInAppMessageViewCounter == 1 || mockDelegate.createInAppMessageViewCounter == 2)
-        // but only 1 should be shown
-        #expect(mockWindow.addSubviewCounter == 1)
-
-        manager.onInAppMessageReceived(messages: [message!])
-
-        #expect(mockDelegate.onNewInAppMessageCounter == 3)
-        // after the view is shown, creating view also shouldn've be called
-        #expect(mockDelegate.createInAppMessageViewCounter == 2)
-        #expect(mockWindow.addSubviewCounter == 1)
+        @Test("if there is already an IAM, don't show another")
+        @MainActor
+        @available(iOS 13.0, *)
+        func InAppMessageViewAlreadyExist() async throws {
+            let manager = RadarInAppMessageManager()
+            let mockDelegate = MockRadarInAppMessageDelegate(manager: manager)
+            manager.setDelegate(mockDelegate)
+            let mockWindow = MockWindow()
+            manager.getKeyWindow = { return mockWindow }
+            
+            mockDelegate.showInAppMessage = true
+            
+            manager.onInAppMessageReceived(messages: [message!])
+            manager.onInAppMessageReceived(messages: [message!])
+            
+            await mockWindow.waitForSubviewAddition()
+            
+            #expect(mockDelegate.onNewInAppMessageCounter == 2)
+            // 2 views could be created since creating the view is async
+            #expect(mockDelegate.createInAppMessageViewCounter == 1 || mockDelegate.createInAppMessageViewCounter == 2)
+            // but only 1 should be shown
+            #expect(mockWindow.addSubviewCounter == 1)
+            
+            manager.onInAppMessageReceived(messages: [message!])
+            
+            #expect(mockDelegate.onNewInAppMessageCounter == 3)
+            // after the view is shown, creating view also shouldn've be called
+            #expect(mockDelegate.createInAppMessageViewCounter == 2)
+            #expect(mockWindow.addSubviewCounter == 1)
+        }
     }
 }
--- RadarSDKTests/RadarSDKTests.swift
+++ RadarSDKTests/RadarSDKTests.swift
@@ -10,48 +10,52 @@ import Testing
 import CoreLocation
 @testable import RadarSDK
 
+@Suite
+struct RadarSwiftParallelTests {
 
-struct RadarSdkTests {
-
-    @Test(.timeLimit(.minutes(1)))
-    func mockTracking() async throws {
-        let mockSession = MockURLSession()
-        let mockClient = RadarAPIClient(apiHelper: RadarApiHelper(session: mockSession))
-        let radar = Radar_Swift(apiClient: mockClient)
+    @Suite
+    struct RadarSDKTests {
         
-        guard let routeResponse = RadarTestUtilsSwift.data(fromResource: "route_distance") else {
-            Issue.record("invalid resource 'route_distance'")
-            return
-        }
-        mockSession.on({ request in
-            request.url?.absoluteString.starts(with: "\(RadarSettings.host)/v1/route/distance") == true
-        }, routeResponse)
-        
-        
-        guard let routeResponse = RadarTestUtilsSwift.data(fromResource: "track") else {
-            Issue.record("invalid resource 'track'")
-            return
-        }
-        mockSession.on({ request in
-            request.url?.absoluteString.starts(with: "\(RadarSettings.host)/v1/track") == true
-        }, routeResponse)
-        
-
-        let origin = CLLocation(latitude: 40.78382, longitude: -73.97536)
-        let destination = CLLocation(latitude: 40.70390, longitude: -73.98670)
-        let steps = 20
-
-        var stepCount = 0
-        
-        await radar.mockTracking(origin: origin, destination: destination, mode: .car, steps: 20, interval: 1, onTrack: { result in
-            stepCount += 1
+        @Test(.timeLimit(.minutes(1)))
+        func mockTracking() async throws {
+            let mockSession = MockURLSession()
+            let mockClient = RadarAPIClient(apiHelper: RadarApiHelper(session: mockSession))
+            let radar = Radar_Swift(apiClient: mockClient)
             
-            #expect(result["status"] as? Int == RadarStatus.success.rawValue)
-            #expect(result["location"] as? CLLocation != nil)
-            #expect(result["user"] != nil)
-            #expect(result["events"] != nil)
-        })
-
-        #expect(stepCount == steps, "Expected \(steps) callbacks but got \(stepCount)")
+            guard let routeResponse = RadarTestUtilsSwift.data(fromResource: "route_distance") else {
+                Issue.record("invalid resource 'route_distance'")
+                return
+            }
+            mockSession.on({ request in
+                request.url?.absoluteString.starts(with: "\(RadarSettings.host)/v1/route/distance") == true
+            }, routeResponse)
+            
+            
+            guard let routeResponse = RadarTestUtilsSwift.data(fromResource: "track") else {
+                Issue.record("invalid resource 'track'")
+                return
+            }
+            mockSession.on({ request in
+                request.url?.absoluteString.starts(with: "\(RadarSettings.host)/v1/track") == true
+            }, routeResponse)
+            
+            
+            let origin = CLLocation(latitude: 40.78382, longitude: -73.97536)
+            let destination = CLLocation(latitude: 40.70390, longitude: -73.98670)
+            let steps = 20
+            
+            var stepCount = 0
+            
+            await radar.mockTracking(origin: origin, destination: destination, mode: .car, steps: 20, interval: 1, onTrack: { result in
+                stepCount += 1
+                
+                #expect(result["status"] as? Int == RadarStatus.success.rawValue)
+                #expect(result["location"] as? CLLocation != nil)
+                #expect(result["user"] != nil)
+                #expect(result["events"] != nil)
+            })
+            
+            #expect(stepCount == steps, "Expected \(steps) callbacks but got \(stepCount)")
+        }
     }
 }
