9b50b5ed57574af5ca27a512a0070775e04ee2ae
Steven Popovich <stevepopovich8@gmail.com>
2026-04-23 12:21:37 -0500

    Fix missing header file

--- RadarSDK/RadarSdkConfiguration.h
+++ RadarSDK/RadarSdkConfiguration.h
@@ -33,6 +33,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (BOOL)bufferGeofenceEntries;
 - (BOOL)bufferGeofenceExits;
 - (BOOL)stopDetection;
+- (BOOL)useVerifiedHostFailover;
 - (instancetype)initWithDict:(NSDictionary *_Nullable)dict;
 - (NSDictionary *)dictionaryValue;
 @end
