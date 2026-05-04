1c828923bb9f4c1df5ef3fc17e0097df9ad56863
ShiCheng Lu <shicheng.lu@radar.com>
2026-05-04 16:46:45 -0400

    example

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -36,11 +36,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIWindowSceneDelegate, UN
         radarInitializeOptions.autoLogNotificationConversions = true
         radarInitializeOptions.autoHandleNotificationDeepLinks = true
         radarInitializeOptions.silentPush = true
-        
-        UserDefaults.standard.set("https://bailey-nonnebulous-nonaccidentally.ngrok-free.dev", forKey: "radar-verifiedHost")
-        UserDefaults.standard.set("https://bailey-nonnebulous-nonaccidentally.ngrok-free.dev", forKey: "radar-host")
 
-        Radar.setAppGroup(nil)
+        Radar.setAppGroup("group.waypoint.data")
         Radar.initialize(publishableKey: "prj_test_pk_0000000000000000000000000000000000000000", options: radarInitializeOptions)
         Radar.setMetadata([ "foo": "bar" ])
         Radar.setDelegate(self)
