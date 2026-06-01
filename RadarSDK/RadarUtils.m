160eff694aa8e24b2d0425e8f245a4fb4bbfb0e4
Steven Popovich <stevepopovich8@gmail.com>
2026-06-01 11:34:19 -0500

    bump version for beta (will revert)

--- RadarSDK/RadarUtils.m
+++ RadarSDK/RadarUtils.m
@@ -47,7 +47,7 @@ static NSDateFormatter *_isoDateFormatter;
 }
 
 + (NSString *)sdkVersion {
-    return @"3.34.0"; // SDK VERSION
+    return @"3.34.0-beta.4"; // SDK VERSION
 }
 
 + (NSString *)deviceId {
