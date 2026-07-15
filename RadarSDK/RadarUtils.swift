eb5e43efeef2d105c394885ec21090db86ee7a32
ShiCheng Lu <shichenglu0708@gmail.com>
2026-07-15 12:52:11 -0400

    bump version to 3.38.0 (#651)

--- RadarSDK/RadarUtils.swift
+++ RadarSDK/RadarUtils.swift
@@ -45,7 +45,7 @@ class RadarUtils: NSObject {
 
     static let country = Locale.current.regionCode
     static let timeZoneOffset = NSNumber(value: TimeZone.current.secondsFromGMT())
-    static let sdkVersion = "3.37.1"
+    static let sdkVersion = "3.38.0"
 
     static var deviceId: String? {
         get async {
