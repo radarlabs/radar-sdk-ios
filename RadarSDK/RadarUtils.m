8b058609162f83a7d3845c53b6a6d7c365a61e36
Steven Popovich <stevepopovich8@gmail.com>
2026-07-29 10:24:44 -0500

    Bump versions

--- RadarSDK/RadarUtils.m
+++ RadarSDK/RadarUtils.m
@@ -47,7 +47,7 @@ static NSDateFormatter *_isoDateFormatter;
 }
 
 + (NSString *)sdkVersion {
-    return @"3.38.0"; // SDK VERSION
+    return @"3.39.0"; // SDK VERSION
 }
 
 + (NSString *)deviceId {
