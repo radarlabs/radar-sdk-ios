64328fbf145146d825bca4e5df1c8efae5ee6a2d
Steven Popovich <stevepopovich8@gmail.com>
2026-08-10 14:18:24 -0500

    better comments

--- RadarSDK/RadarLocationManager.m
+++ RadarSDK/RadarLocationManager.m
@@ -1474,8 +1474,6 @@ static NSString *const kSyncBeaconUUIDIdentifierPrefix = @"radar_uuid_";
 
 - (void)locationManager:(CLLocationManager *)manager didVisit:(CLVisit *)visit {
     if ([RadarSettings sdkConfiguration].useSwiftLocationManager) {
-        // The Swift twin owns the whole guard, including the nil-location check, so that this
-        // ObjC body can be deleted wholesale at cutover.
         if (![RadarLocationManagerSwift shouldHandleVisit:visit location:manager.location]) {
             return;
         }
