58b3bdc9260457d0fcfa9a7fe9bf8e52fde60a07
Steven Popovich <stevepopovich8@gmail.com>
2026-08-11 14:48:05 -0500

    Revert "Port location update routing to Swift"
    This reverts commit 07d2fc42f84f17a09fd00bb5a09ebdfb77d01c70.

--- RadarSDK/RadarLocationManagerSwift.h
+++ RadarSDK/RadarLocationManagerSwift.h
@@ -12,7 +12,6 @@
 #import <CoreLocation/CoreLocation.h>
 #import <Foundation/Foundation.h>
 
-#import "Radar.h"
 #import "RadarBeacon.h"
 #import "RadarGeofence.h"
 #import "RadarMeta.h"
@@ -49,9 +48,6 @@ NS_ASSUME_NONNULL_BEGIN
 + (void)applyRemoteTrackingOptions:(nullable RadarMeta *)meta;
 
 + (BOOL)shouldHandleRegionWithIdentifier:(NSString *)identifier action:(NSString *)action;
-
-+ (RadarLocationSource)locationSourceForUpdates:(nullable NSArray<CLLocation *> *)updates
-                         completionHandlerCount:(NSUInteger)completionHandlerCount;
 
 + (void)didUpdateHeading:(CLHeading *)newHeading;
 + (void)didChangeAuthorizationStatus:(CLAuthorizationStatus)status;
