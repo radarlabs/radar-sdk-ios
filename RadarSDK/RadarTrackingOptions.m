6cfbb2366bb1ea218912b7b8426254d3cb25d261
Alan <alan.charles@radar.com>
2026-09-02 16:13:07 -0700

    add comments

--- RadarSDK/RadarTrackingOptions.m
+++ RadarSDK/RadarTrackingOptions.m
@@ -7,6 +7,11 @@
 
 #import "RadarTrackingOptions.h"
 
+// Swift's @objc @implementation cannot implement Objective-C factory
+// convenience initializers. Keep this category as a compatibility shim so
+// +trackingOptionsFromDictionary: remains available to Objective-C callers
+// and continues importing into Swift as init(from:).
+
 @interface RadarTrackingOptions (SwiftFactory)
 
 + (RadarTrackingOptions *_Nullable)radar_trackingOptionsFromDictionary:(NSDictionary *_Nullable)dictionary;
