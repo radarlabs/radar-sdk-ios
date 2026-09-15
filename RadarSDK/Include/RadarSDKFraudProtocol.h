c7f2cb04eba6193df2109024f46b9967de8d385e
Alan <alan.charles@radar.com>
2026-09-15 16:56:04 -0700

    remove unused declaration

--- RadarSDK/Include/RadarSDKFraudProtocol.h
+++ RadarSDK/Include/RadarSDKFraudProtocol.h
@@ -21,9 +21,6 @@ typedef void (^RadarFraudPayloadCallback)(NSDictionary<NSString *, id> *_Nullabl
 
 - (void)getFraudPayloadWithOptions:(NSDictionary<NSString *, id> *)options completionHandler:(RadarFraudPayloadCallback)completionHandler;
 
-- (void)getEncryptedFraudPayloadWithOptions:(NSDictionary<NSString *, id> *)options
-                          completionHandler:(RadarFraudPayloadCallback)completionHandler;
-
 - (void)setSharingDidChangeHandler:(nullable void (^)(BOOL sharing))handler;
 
 @end
