cf42d1845ed8489ad3d5db5b4a3190c707b4401b
Steven Popovich <stevepopovich8@gmail.com>
2026-09-21 19:29:32 -0500

    Remove order-dependent Radar test setup assertion

--- RadarSDKTests/RadarSDKTests.m
+++ RadarSDKTests/RadarSDKTests.m
@@ -283,10 +283,6 @@ static NSString *const kPublishableKey = @"prj_test_pk_0000000000000000000000000
     AssertRouteOk(routes.car);
 }
 
-+ (void)setUp {
-    XCTAssertFalse([Radar isInitialized]);
-}
-
 - (void)setUp {
     [super setUp];
     [Radar initializeWithPublishableKey:kPublishableKey];
