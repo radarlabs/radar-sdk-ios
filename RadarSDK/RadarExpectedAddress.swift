c0c0e532ea40b969d331e8fe44e5b1e779166c09
ShiCheng Lu <shicheng.lu@radar.com>
2026-09-25 16:41:54 -0400

    whitest of spaces

--- RadarSDK/RadarExpectedAddress.swift
+++ RadarSDK/RadarExpectedAddress.swift
@@ -52,38 +52,38 @@ public enum RadarExpectedAddressConfidence: Int {
 @objc(RadarExpectedAddress)
 public final class RadarExpectedAddress: NSObject {
     let data: RadarExpectedAddressData
-    
+
     /**
      The user's expected address, as passed to `setExpectedAddress:`.
      */
     @objc public var expectedAddress: String { data.expectedAddress }
-    
+
     /**
      The formatted expected address, as geocoded by Radar. May be `nil` if the expected address could not be geocoded.
      */
     @objc public var formattedAddress: String? { data.formattedAddress }
-    
+
     /**
      The latitude of the geocoded expected address. May be `nil` if the expected address could not be geocoded.
      */
     @objc public var latitude: NSNumber? { data.latitude.map(NSNumber.init(value:)) }
-    
+
     /**
      The longitude of the geocoded expected address. May be `nil` if the expected address could not be geocoded.
      */
     @objc public var longitude: NSNumber? { data.longitude.map(NSNumber.init(value:)) }
-    
+
     /**
      A boolean indicating whether the user is at their expected address.
      */
     @objc public var atAddress: Bool { data.atAddress }
-    
+
     /**
      The confidence of the match between the user's location and their expected address. May be
      `RadarExpectedAddressConfidenceUnknown` if confidence is not available.
      */
     @objc public var confidence: RadarExpectedAddressConfidence { data.confidence.toObjC() }
-    
+
     /**
      The distance in meters between the user's location and their expected address. May be `nil` if the expected
      address could not be geocoded.
